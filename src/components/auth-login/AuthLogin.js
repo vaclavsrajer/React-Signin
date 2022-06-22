851d483877f0fbad16941b6bae6c2a5b7a13567d
vaclavsrajer <venda1997@gmail.com>
2022-06-22 08:41:44 +0200

    functionality

--- src/components/auth-login/AuthLogin.js
+++ src/components/auth-login/AuthLogin.js
@@ -3,14 +3,21 @@ import apple from "../../images/apple.svg";
 import google from "../../images/google.svg";
 
 function AuthLogin() {
+  const openInNewTab = (url) => {
+    window.open(url, "_blank", "noopener,noreferrer");
+  };
+
   return (
     <div className="auth-button-container">
-      <button className="auth-button">
+      <button
+        onClick={() => openInNewTab("https://google.com")}
+        className="auth-button"
+      >
         <img alt="google" src={google} />
         Log in with Google
       </button>
 
-      <button className="auth-button">
+      <button onClick={() => openInNewTab("https://www.apple.com/")} className="auth-button">
         <img alt="apple" src={apple} />
         Log in with Apple
       </button>
--- src/components/sign-up/SignUp.js
+++ src/components/sign-up/SignUp.js
@@ -1,7 +1,29 @@
-import "./SignUp.css";
+import React, { useState } from "react";
 import AuthLogin from "../auth-login/AuthLogin";
 
+import "./SignUp.css";
+
 function SignUp() {
+  const [registerData, setRegisterData] = useState({
+    email: "",
+    name: "",
+    password: "",
+    date: "",
+    gender: "",
+  });
+
+  const dateRegex =
+    /^(?=\\d{2}([-.,\\/])\\d{2}\\1\\d{4}$)(?:0[1-9]|1\\d|[2][0-8]|29(?!.02.(?!(?!(?:[02468][1-35-79]|[13579][0-13-57-9])00)\\d{2}(?:[02468][048]|[13579][26])))|30(?!.02)|31(?=.(?:0[13578]|10|12))).(?:0[1-9]|1[012]).\\d{4}$/;
+
+  function handleChange(e) {
+    const { name, value, type, id } = e.target;
+    console.log(e.target.value);
+    setRegisterData(prevValue => ({
+      ...prevValue,
+      [name]: name === "date" ? (value.match(dateRegex) ? value : "") : type === "radio" ? id : value
+    }));
+  }
+
   return (
     <div className="sing-up-container">
       <AuthLogin />
@@ -11,40 +33,50 @@ function SignUp() {
           type="email"
           name="email"
           placeholder="Your Email"
-          value={""}
+          value={registerData.email}
+          onChange={handleChange}
         />
         <input
           className="normal-input"
           type="text"
           name="name"
           placeholder="Your Name"
-          value={""}
+          value={registerData.name}
+          onChange={handleChange}
         />
         <input
           className="normal-input"
           type="password"
           name="password"
           placeholder="Create Password"
-          value={""}
+          value={registerData.password}
+          onChange={handleChange}
         />
         <div className="sign-up-special-inputs">
-          <input className="sign-up-date" type="text" name="date" placeholder="DD/MM/YY" />
+          <input
+            className="sign-up-date"
+            type="text"
+            name="date"
+            placeholder="DD/MM/YY"
+            value={registerData.date}
+            onChange={handleChange}
+          />
           <fieldset className="sign-up-checkboxes">
             <input
               className="sign-up-special-input"
               type="radio"
-              name="email"
+              name="gender"
               id="male"
-              // checked={formData.employment === "unemployed"}
-              // onChange={handleChange}
+              checked={registerData.gender === "male"}
+              onChange={handleChange}
             />
             <label htmlFor="male">Male</label>
             <input
               type="radio"
-              name="email"
+              name="gender"
               id="female"
-              // checked={formData.employment === "unemployed"}
-              // onChange={handleChange}
+              checked={registerData.gender === "female"}
+              onChange={handleChange}
             />
             <label htmlFor="female">Female</label>
           </fieldset>
